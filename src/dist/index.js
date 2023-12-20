7ba6c09701b6b59e9a047b67fc6ffecb62b829ad
skoanton <antonhakanssonn@gmail.com>
2023-12-20 14:11:00 +0100

    displayperson

--- src/dist/index.js
+++ src/dist/index.js
@@ -14,13 +14,34 @@ const planetUrl = 'planets/';
 let fetchedPersons = [];
 let fetchedPlanets = [];
 let homeWorldUrl = '';
+//Person element
+const personName = document.getElementById("personName");
+const personHeight = document.getElementById("personHeight");
+const personMass = document.getElementById("personMass");
+const personHairColor = document.getElementById("personHairColor");
+const personSkinColor = document.getElementById("personSkinColor");
+const personEyeColor = document.getElementById("personEyeColor");
+const personBirthYear = document.getElementById("personBirthYear");
+const personGender = document.getElementById("personGender");
+let curentPerson = 0;
+// Planet element
+const planetName = document.getElementById("planetName");
+const planetRotation = document.getElementById("planetRotation");
+const planetDiameter = document.getElementById("planetDiameter");
+const planetClimate = document.getElementById("planetCLimate");
+const planetGravity = document.getElementById("planetGravity");
+const planetTerrain = document.getElementById("planetTerrain");
 function fetchInfo() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const response = yield fetch(`${url}${peopleUrl}`);
             if (response.status === 200) {
                 const data = yield response.json();
-                fetchedPersons = data;
+                console.log(data.results);
+                /* fetchedPersons?.push(data.results); */
+                fetchedPersons = data.results;
+                console.log(fetchedPersons);
+                return fetchedPersons;
             }
             else {
                 throw Error(String(response.status));
@@ -28,6 +49,7 @@ function fetchInfo() {
         }
         catch (error) {
             console.log(error);
+            return Promise.reject(error);
         }
         try {
             const response = yield fetch(`${url}${planetUrl}`);
@@ -45,3 +67,38 @@ function fetchInfo() {
     });
 }
 fetchInfo();
+displayPersonInfo();
+displayPlanetInfo();
+function displayPersonInfo() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let person = yield fetchInfo();
+        if (person && person.length > 0) {
+            personName.innerHTML = person[curentPerson].name;
+            personHeight.innerHTML = person[curentPerson].height;
+            personMass.innerHTML = person[curentPerson].mass;
+            personHairColor.innerHTML = person[curentPerson].hair_color;
+            personEyeColor.innerHTML = person[curentPerson].eye_color;
+            personBirthYear.innerHTML = person[curentPerson].birth_year;
+            personGender.innerHTML = person[curentPerson].gender;
+        }
+        else {
+            console.error("fel att lägga till personer");
+        }
+    });
+}
+function displayPlanetInfo() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let planet = yield fetchInfo();
+        if (planet && planet.length > 0) {
+            planetName.innerHTML = fetchedPlanets[1].name;
+            planetRotation.innerHTML = fetchedPlanets[1].rotation_speed;
+            planetDiameter.innerHTML = fetchedPlanets[1].diameter;
+            planetClimate.innerHTML = fetchedPlanets[1].climate;
+            planetGravity.innerHTML = fetchedPlanets[1].gravity;
+            planetTerrain.innerHTML = fetchedPlanets[1].terrain;
+        }
+        else {
+            console.error("fel att lägga till planeter");
+        }
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,75 @@
 const url: string = 'https://swapi.dev/api/'
 const peopleUrl: string = 'people/'
 const planetUrl: string = 'planets/'
-let fetchedPersons: CharachterInfo[] = []
+let fetchedPersons: CharacterInfo[] | null = []
 let fetchedPlanets: PlanetInfo[] = []
 let homeWorldUrl: string = ''
 
-interface CharachterInfo{
-    name:string,
-    height:string,
-    mass:string,
-    hair_color:string,
-    skin_color:string,
-    eye_color:string,
-    birth_year:string,
-    gender:string,
-    homeworld:string,
-
+interface CharacterInfo{
+   
+        name:string,
+        height:string,
+        mass:string,
+        hair_color:string,
+        skin_color:string,
+        eye_color:string,
+        birth_year:string,
+        gender:string,
+        homeworld:string,
+   
 }
 
 interface PlanetInfo{
         name:string,
         rotation_speed:string,
+        diameter:string,
         orbital_speed:string,
         climate:string;
         gravity:string,
         terrain:string,
 }
 
+//Person element
+const personName:HTMLElement | null = document.getElementById("personName");
+const personHeight:HTMLElement | null = document.getElementById("personHeight");
+const personMass:HTMLElement | null = document.getElementById("personMass");
+const personHairColor:HTMLElement | null = document.getElementById("personHairColor");
+const personSkinColor:HTMLElement | null = document.getElementById("personSkinColor");
+const personEyeColor:HTMLElement | null = document.getElementById("personEyeColor");
+const personBirthYear:HTMLElement | null = document.getElementById("personBirthYear");
+const personGender:HTMLElement | null = document.getElementById("personGender");
+let curentPerson = 0;
 
 
+// Planet element
+const planetName:HTMLElement | null = document.getElementById("planetName");
+const planetRotation:HTMLElement | null = document.getElementById("planetRotation");
+const planetDiameter:HTMLElement | null = document.getElementById("planetDiameter");
+const planetClimate:HTMLElement | null = document.getElementById("planetCLimate");
+const planetGravity:HTMLElement | null = document.getElementById("planetGravity");
+const planetTerrain:HTMLElement | null = document.getElementById("planetTerrain");
 
-async function fetchInfo() {
+async function fetchInfo(): Promise<CharacterInfo[]> {
     try {
-        const response = await fetch(`${url}${peopleUrl}`)
+        const response = await fetch(`${url}${peopleUrl}`);
         if (response.status === 200) {
-            const data: CharachterInfo[] = await response.json()
-            fetchedPersons = data
+            const data  = await response.json()
+           console.log(data.results);
+           /* fetchedPersons?.push(data.results); */
+           fetchedPersons = data.results;
+           console.log(fetchedPersons);
+            return fetchedPersons!;
+            
         }
         else {
             throw Error(String(response.status))
+            
         }
     }
     catch(error) {
         console.log(error)
+        return Promise.reject(error);
+        
     }
 
     try {
@@ -57,7 +84,49 @@ async function fetchInfo() {
     }
     catch(error) {
         console.log()
-    }
+    }  
 }
 
-fetchInfo()
+fetchInfo();
+displayPersonInfo();
+displayPlanetInfo();
+
+async function displayPersonInfo(){
+
+   let person = await fetchInfo()
+   if(person && person.length > 0)  {
+    personName!.innerHTML = person![curentPerson].name;
+    personHeight!.innerHTML = person![curentPerson].height;
+    personMass!.innerHTML = person![curentPerson].mass;
+    personHairColor!.innerHTML = person![curentPerson].hair_color;
+    personEyeColor!.innerHTML = person![curentPerson].eye_color;
+    personBirthYear!.innerHTML = person![curentPerson].birth_year;
+    personGender!.innerHTML = person![curentPerson].gender;
+   }
+      
+    else{
+        console.error("fel att lägga till personer");
+    }
+
+} 
+
+async function displayPlanetInfo(){
+
+let planet = await fetchInfo()
+
+if(planet && planet.length > 0){
+    planetName!.innerHTML = fetchedPlanets[1].name;
+    planetRotation!.innerHTML = fetchedPlanets[1].rotation_speed;
+    planetDiameter!.innerHTML = fetchedPlanets[1].diameter;
+    planetClimate!.innerHTML = fetchedPlanets[1].climate;
+    planetGravity!.innerHTML = fetchedPlanets[1].gravity;
+    planetTerrain!.innerHTML = fetchedPlanets[1].terrain;
+}
+
+else{
+    console.error("fel att lägga till planeter");
+}
+
+    
+
+}
