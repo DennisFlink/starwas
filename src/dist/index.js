8b091c1fd4eacae85560badadf2254d6378601ce
Gustav Syrén <g.e.syren@gmail.com>
2023-12-20 14:10:34 +0100

    fetch update

--- src/dist/index.js
+++ src/dist/index.js
@@ -14,13 +14,23 @@ const planetUrl = 'planets/';
 let fetchedPersons = [];
 let fetchedPlanets = [];
 let homeWorldUrl = '';
-function fetchInfo() {
+const planetName = document.querySelector('.planet-name');
+const rotation = document.querySelector('.rotation');
+const diameter = document.querySelector('.diameter');
+const climate = document.querySelector('.climate');
+const gravity = document.querySelector('.gravity');
+const terrain = document.querySelector('.terrain');
+window.addEventListener('load', function () {
+    fetchAllPepole();
+    fetchAllPlanets();
+});
+function fetchPeople(pageNumber) {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield fetch(`${url}${peopleUrl}`);
+            const response = yield fetch(`${url}${peopleUrl}?1=&page=${pageNumber}`);
             if (response.status === 200) {
                 const data = yield response.json();
-                fetchedPersons = data;
+                return data.results;
             }
             else {
                 throw Error(String(response.status));
@@ -28,12 +38,17 @@ function fetchInfo() {
         }
         catch (error) {
             console.log(error);
+            return [];
         }
+    });
+}
+function fetchPlanets(pageNumber) {
+    return __awaiter(this, void 0, void 0, function* () {
         try {
-            const response = yield fetch(`${url}${planetUrl}`);
+            const response = yield fetch(`${url}${planetUrl}?1=&page=${pageNumber}`);
             if (response.status === 200) {
                 const data = yield response.json();
-                fetchedPlanets = data;
+                return data.results;
             }
             else {
                 throw Error(String(response.status));
@@ -41,7 +56,29 @@ function fetchInfo() {
         }
         catch (error) {
             console.log();
+            return [];
         }
     });
 }
-fetchInfo();
+function fetchAllPepole() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let pageNumber = 1;
+        let currentPagePersons = [];
+        do {
+            currentPagePersons = yield fetchPeople(pageNumber);
+            fetchedPersons = fetchedPersons.concat(currentPagePersons);
+            pageNumber++;
+        } while (pageNumber < 10);
+    });
+}
+function fetchAllPlanets() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let pageNumber = 1;
+        let currentPagePlanets = [];
+        do {
+            currentPagePlanets = yield fetchPlanets(pageNumber);
+            fetchedPlanets = fetchedPlanets.concat(currentPagePlanets);
+            pageNumber++;
+        } while (pageNumber < 7);
+    });
+}
--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,61 @@
 const url: string = 'https://swapi.dev/api/'
 const peopleUrl: string = 'people/'
 const planetUrl: string = 'planets/'
-let fetchedPersons: CharachterInfo[] = []
+let fetchedPersons: CharacterInfo[] = []
 let fetchedPlanets: PlanetInfo[] = []
 let homeWorldUrl: string = ''
 
-interface CharachterInfo{
-    name:string,
-    height:string,
-    mass:string,
-    hair_color:string,
-    skin_color:string,
-    eye_color:string,
-    birth_year:string,
-    gender:string,
-    homeworld:string,
 
+interface CharacterInfo {
+    name: string;
+    height: string;
+    mass: string;
+    hair_color: string;
+    skin_color: string;
+    eye_color: string;
+    birth_year: string;
+    gender: string;
+    homeworld: string;
+    films: string[];
+    species: string[];
+    vehicles: string[];
+    starships: string[];
+    created: string;
+    edited: string;
+    url: string;
 }
 
 interface PlanetInfo{
+    results:{
         name:string,
         rotation_speed:string,
         orbital_speed:string,
         climate:string;
         gravity:string,
         terrain:string,
+        url:string
+    }
 }
 
+const planetName = document.querySelector('.planet-name')
+const rotation = document.querySelector('.rotation')
+const diameter = document.querySelector('.diameter')
+const climate = document.querySelector('.climate')
+const gravity = document.querySelector('.gravity')
+const terrain = document.querySelector('.terrain')
 
+window.addEventListener('load', function() {
+    fetchAllPepole()
+    fetchAllPlanets()
+})
 
-
-async function fetchInfo() {
+async function fetchPeople(pageNumber: number): Promise<CharacterInfo[]> {
     try {
-        const response = await fetch(`${url}${peopleUrl}`)
+        const response = await fetch(`${url}${peopleUrl}?1=&page=${pageNumber}`)
+
         if (response.status === 200) {
-            const data: CharachterInfo[] = await response.json()
-            fetchedPersons = data
+            const data = await response.json()
+            return data.results
         }
         else {
             throw Error(String(response.status))
@@ -43,13 +63,18 @@ async function fetchInfo() {
     }
     catch(error) {
         console.log(error)
+        return[]
     }
 
+}
+
+async function fetchPlanets(pageNumber: number): Promise<PlanetInfo[]> {
+    
     try {
-        const response = await fetch(`${url}${planetUrl}`)
+        const response = await fetch(`${url}${planetUrl}?1=&page=${pageNumber}`)
         if (response.status === 200) {
-            const data: PlanetInfo[] = await response.json()
-            fetchedPlanets = data
+            const data = await response.json()
+            return data.results
         }
         else {
             throw Error(String(response.status))
@@ -57,7 +82,33 @@ async function fetchInfo() {
     }
     catch(error) {
         console.log()
+        return[]
     }
 }
 
-fetchInfo()
+
+async function fetchAllPepole(): Promise<void> {
+    let pageNumber = 1;
+    let currentPagePersons: CharacterInfo[] = []
+    
+    do {
+        currentPagePersons = await fetchPeople(pageNumber)
+        fetchedPersons = fetchedPersons.concat(currentPagePersons)
+        pageNumber++
+    }
+    while (pageNumber < 10)
+}
+
+async function fetchAllPlanets(): Promise<void> {
+    let pageNumber = 1;
+    let currentPagePlanets: PlanetInfo[] = []
+
+    do {
+        currentPagePlanets = await fetchPlanets(pageNumber)
+        fetchedPlanets = fetchedPlanets.concat(currentPagePlanets)
+        pageNumber++
+    }
+    while (pageNumber < 7)
+}
+
+
