8b5ff5268ab447d2c66a069e38031df51fe3a3f6
DennisFlink <dennis.sabriev@outlook.com>
2023-12-21 14:24:58 +0100

    pages

--- src/index.ts
+++ src/index.ts
@@ -1,119 +1,110 @@
-const url: string = 'https://swapi.dev/api/'
-const peopleUrl: string = 'people/'
-const planetUrl: string = 'planets/'
-let fetchedPersons: CharacterInfo[] | null = []
-let fetchedPlanets: PlanetInfo[] = []
-let homeWorldUrl: string = ''
-
-
+const url: string = "https://swapi.dev/api/";
+const peopleUrl: string = "people/";
+const planetUrl: string = "planets/";
+let fetchedPersons: CharacterInfo[] | null = [];
+let fetchedPlanets: PlanetInfo[] = [];
+let homeWorldUrl: string = "";
 
 interface CharacterInfo {
-    name: string;
-    height: string;
-    mass: string;
-    hair_color: string;
-    skin_color: string;
-    eye_color: string;
-    birth_year: string;
-    gender: string;
-    homeworld: string;
-    films: string[];
-    species: string[];
-    vehicles: string[];
-    starships: string[];
-    created: string;
-    edited: string;
-    url: string;
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
+  homeworld: string;
+  films: string[];
+  species: string[];
+  vehicles: string[];
+  starships: string[];
+  created: string;
+  edited: string;
+  url: string;
 }
 
-interface PlanetInfo{
-        name:string,
-        rotation_speed:string,
-        diameter:string,
-        orbital_speed:string,
-        climate:string;
-        gravity:string,
-        terrain:string,
+interface PlanetInfo {
+  name: string;
+  rotation_speed: string;
+  diameter: string;
+  orbital_speed: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
 }
 
 //Person element
-const personName:HTMLElement | null = document.getElementById("personName");
-const personHeight:HTMLElement | null = document.getElementById("personHeight");
-const personMass:HTMLElement | null = document.getElementById("personMass");
-const personHairColor:HTMLElement | null = document.getElementById("personHairColor");
-const personSkinColor:HTMLElement | null = document.getElementById("personSkinColor");
-const personEyeColor:HTMLElement | null = document.getElementById("personEyeColor");
-const personBirthYear:HTMLElement | null = document.getElementById("personBirthYear");
-const personGender:HTMLElement | null = document.getElementById("personGender");
+const personName: HTMLElement | null = document.getElementById("personName");
+const personHeight: HTMLElement | null =
+  document.getElementById("personHeight");
+const personMass: HTMLElement | null = document.getElementById("personMass");
+const personHairColor: HTMLElement | null =
+  document.getElementById("personHairColor");
+const personSkinColor: HTMLElement | null =
+  document.getElementById("personSkinColor");
+const personEyeColor: HTMLElement | null =
+  document.getElementById("personEyeColor");
+const personBirthYear: HTMLElement | null =
+  document.getElementById("personBirthYear");
+const personGender: HTMLElement | null =
+  document.getElementById("personGender");
 let curentPerson = 0;
 
-
 // Planet element
-const planetName:HTMLElement | null = document.getElementById("planetName");
-const planetRotation:HTMLElement | null = document.getElementById("planetRotation");
-const planetDiameter:HTMLElement | null = document.getElementById("planetDiameter");
-const planetClimate:HTMLElement | null = document.getElementById("planetCLimate");
-const planetGravity:HTMLElement | null = document.getElementById("planetGravity");
-const planetTerrain:HTMLElement | null = document.getElementById("planetTerrain");
+const planetName: HTMLElement | null = document.getElementById("planetName");
+const planetRotation: HTMLElement | null =
+  document.getElementById("planetRotation");
+const planetDiameter: HTMLElement | null =
+  document.getElementById("planetDiameter");
+const planetClimate: HTMLElement | null =
+  document.getElementById("planetCLimate");
+const planetGravity: HTMLElement | null =
+  document.getElementById("planetGravity");
+const planetTerrain: HTMLElement | null =
+  document.getElementById("planetTerrain");
 
-
-window.addEventListener('load', function() {
-    fetchAllPepole()
-    fetchAllPlanets()
-    
-})
+window.addEventListener("load", function () {
+  fetchAllPepole();
+  fetchAllPlanets();
+});
 
 async function fetchPeople(pageNumber: number): Promise<CharacterInfo[]> {
+  try {
+    const response = await fetch(`${url}${peopleUrl}?1=&page=${pageNumber}`);
 
-    try {
-        
-        const response = await fetch(`${url}${peopleUrl}?1=&page=${pageNumber}`)
-
-        if (response.status === 200) {
-            const data  = await response.json()
-            console.log("hej");
-            return data.results
-        }
-        else {
-            throw Error(String(response.status))
-            
-        }
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log("hej");
+      return data.results;
+    } else {
+      throw Error(String(response.status));
     }
-    catch(error) {
-        console.log(error)
-        return Promise.reject(error);
-        
-        return[]
-    }
+  } catch (error) {
+    console.log(error);
+    return Promise.reject(error);
 
+    return [];
+  }
 }
 
 async function fetchPlanets(pageNumber: number): Promise<PlanetInfo[]> {
-    
-    try {
-        const response = await fetch(`${url}${planetUrl}?1=&page=${pageNumber}`)
-        if (response.status === 200) {
-            const data = await response.json()
-            return data.results
-        }
-        else {
-            throw Error(String(response.status))
-        }
+  try {
+    const response = await fetch(`${url}${planetUrl}?1=&page=${pageNumber}`);
+    if (response.status === 200) {
+      const data = await response.json();
+      return data.results;
+    } else {
+      throw Error(String(response.status));
     }
-    catch(error) {
-        console.log()
-    }  
-        return[]
-    }
+  } catch (error) {
+    console.log();
+  }
+  return [];
+}
 
-
-
-
-
-function displayPersonInfo(){
-
-    
-   if(fetchedPersons && fetchedPersons.length > 0)  {
+function displayPersonInfo() {
+  if (fetchedPersons && fetchedPersons.length > 0) {
     personName!.innerHTML = fetchedPersons![curentPerson].name;
     personHeight!.innerHTML = fetchedPersons![curentPerson].height;
     personMass!.innerHTML = fetchedPersons![curentPerson].mass;
@@ -121,58 +112,148 @@ function displayPersonInfo(){
     personEyeColor!.innerHTML = fetchedPersons![curentPerson].eye_color;
     personBirthYear!.innerHTML = fetchedPersons![curentPerson].birth_year;
     personGender!.innerHTML = fetchedPersons![curentPerson].gender;
-   }
-      
-    else{
-        console.error("fel att lägga till personer");
-    }
+  } else {
+    console.error("fel att lägga till personer");
+  }
+}
 
-} 
-
-function displayPlanetInfo(){
-
-    if(fetchPlanets && fetchPlanets.length > 0){
-        planetName!.innerHTML = fetchedPlanets[1].name;
-        planetRotation!.innerHTML = fetchedPlanets[1].rotation_speed;
-        planetDiameter!.innerHTML = fetchedPlanets[1].diameter;
-        planetClimate!.innerHTML = fetchedPlanets[1].climate;
-        planetGravity!.innerHTML = fetchedPlanets[1].gravity;
-        planetTerrain!.innerHTML = fetchedPlanets[1].terrain;
-    }
-    
-    else{
-        console.error("fel att lägga till planeter");
-    }
-
-
-
-
+function displayPlanetInfo() {
+  if (fetchPlanets && fetchPlanets.length > 0) {
+    planetName!.innerHTML = fetchedPlanets[1].name;
+    planetRotation!.innerHTML = fetchedPlanets[1].rotation_speed;
+    planetDiameter!.innerHTML = fetchedPlanets[1].diameter;
+    planetClimate!.innerHTML = fetchedPlanets[1].climate;
+    planetGravity!.innerHTML = fetchedPlanets[1].gravity;
+    planetTerrain!.innerHTML = fetchedPlanets[1].terrain;
+  } else {
+    console.error("fel att lägga till planeter");
+  }
 }
 
 async function fetchAllPepole(): Promise<void> {
-    let pageNumber = 1;
-    let currentPagePersons: CharacterInfo[] = []
-    console.log("fetch pepole");
-    do {
-        currentPagePersons = await fetchPeople(pageNumber)
-        fetchedPersons = fetchedPersons!.concat(currentPagePersons)
-        pageNumber++
-    }
-    while (pageNumber < 10)
-    displayPersonInfo();
+  let pageNumber = 1;
+  let currentPagePersons: CharacterInfo[] = [];
+  console.log("fetch pepole");
+  do {
+    currentPagePersons = await fetchPeople(pageNumber);
+    fetchedPersons = fetchedPersons!.concat(currentPagePersons);
+    pageNumber++;
+  } while (pageNumber < 10);
+  displayPersonInfo();
 }
 
 async function fetchAllPlanets(): Promise<void> {
-    let pageNumber = 1;
-    let currentPagePlanets: PlanetInfo[] = []
+  let pageNumber = 1;
+  let currentPagePlanets: PlanetInfo[] = [];
 
-    do {
-        currentPagePlanets = await fetchPlanets(pageNumber)
-        fetchedPlanets = fetchedPlanets.concat(currentPagePlanets)
-        pageNumber++
-    }
-    while (pageNumber < 7)
-    
+  do {
+    currentPagePlanets = await fetchPlanets(pageNumber);
+    fetchedPlanets = fetchedPlanets.concat(currentPagePlanets);
+    pageNumber++;
+  } while (pageNumber < 7);
 }
 
+/* PAGE FUNCTION */
+const paginationNumbers = document.getElementById(
+  "pagination-numbers"
+) as HTMLElement;
+const paginatedList = document.getElementById("characters-list") as HTMLElement;
+const listItems = paginatedList.querySelectorAll(
+  "li"
+) as NodeListOf<HTMLLIElement>;
+const nextButton = document.getElementById("next-button") as HTMLButtonElement;
+const prevButton = document.getElementById("prev-button") as HTMLButtonElement;
 
+const paginationLimit = 10;
+const pageCount = Math.ceil(listItems.length / paginationLimit);
+let currentPage = 1;
+
+const disableButton = (button: any) => {
+  button.classList.add("disabled");
+  button.setAttribute("disabled", true);
+};
+
+const enableButton = (button: any) => {
+  button.classList.remove("disabled");
+  button.removeAttribute("disabled");
+};
+
+const handlePageButtonsStatus = () => {
+  if (currentPage === 1) {
+    disableButton(prevButton);
+  } else {
+    enableButton(prevButton);
+  }
+
+  if (pageCount === currentPage) {
+    disableButton(nextButton);
+  } else {
+    enableButton(nextButton);
+  }
+};
+
+const handleActivePageNumber = () => {
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    button.classList.remove("active");
+    const pageIndex = Number(button.getAttribute("page-index"));
+    if (pageIndex == currentPage) {
+      button.classList.add("active");
+    }
+  });
+};
+
+const appendPageNumber = (index: any) => {
+  const pageNumber = document.createElement("button");
+  pageNumber.className = "pagination-number";
+  pageNumber.innerHTML = index;
+  pageNumber.setAttribute("page-index", index);
+  pageNumber.setAttribute("aria-label", "Page " + index);
+
+  paginationNumbers.appendChild(pageNumber);
+};
+
+const getPaginationNumbers = () => {
+  for (let i = 1; i <= pageCount; i++) {
+    appendPageNumber(i);
+  }
+};
+
+const setCurrentPage = (pageNum: any) => {
+  currentPage = pageNum;
+
+  handleActivePageNumber();
+  handlePageButtonsStatus();
+
+  const prevRange = (pageNum - 1) * paginationLimit;
+  const currRange = pageNum * paginationLimit;
+
+  listItems.forEach((item, index) => {
+    item.classList.add("hidden");
+    if (index >= prevRange && index < currRange) {
+      item.classList.remove("hidden");
+    }
+  });
+};
+
+window.addEventListener("load", () => {
+  getPaginationNumbers();
+  setCurrentPage(1);
+
+  prevButton.addEventListener("click", () => {
+    setCurrentPage(currentPage - 1);
+  });
+
+  nextButton.addEventListener("click", () => {
+    setCurrentPage(currentPage + 1);
+  });
+
+  document.querySelectorAll(".pagination-number").forEach((button) => {
+    const pageIndex = Number(button.getAttribute("page-index"));
+
+    if (pageIndex) {
+      button.addEventListener("click", () => {
+        setCurrentPage(pageIndex);
+      });
+    }
+  });
+});
